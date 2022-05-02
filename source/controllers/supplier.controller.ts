95ec1b0dfb8372f472c609c480d51f257d9cc363
Gusttavo-Larios <eg9653641@gmail.com>
2022-05-02 10:53:11 -0300

    Corrigindo controllers

--- source/controllers/supplier.controller.ts
+++ source/controllers/supplier.controller.ts
@@ -10,18 +10,31 @@ import {
 
 export async function searchAll(request: Request, response: Response) {
   const allSuppliers = await selectAllSupliers();
-  response.status(200).json({ allSuppliers });
+  return response.status(200).json({ allSuppliers });
 }
 
 export async function search(request: Request, response: Response) {
   const { cnpj_number } = request.query as unknown as SupplierInterface;
   const supplier = await selectSuplier(cnpj_number);
-  response.status(200).json({ supplier });
+  return response.status(200).json({ supplier });
 }
 
-export function resgiter(request: Request, response: Response) {
-  insertSupplier(request.body);
-  response.status(200).json({ message: "Fornecedor cadastrado com sucesso." });
+export async function resgiter(request: Request, response: Response) {
+  const supplier: SupplierInterface = request.body;
+
+  const registered_supplier = await selectSuplier(supplier.cnpj_number);
+  const cnpj_already_registered = registered_supplier ? true : false;
+
+  if (cnpj_already_registered) {
+    return response
+      .status(400)
+      .json({ message: "CNPJ já existente no sistema" });
+  }
+
+  insertSupplier(supplier);
+  return response
+    .status(200)
+    .json({ message: "Fornecedor cadastrado com sucesso." });
 }
 
 export async function update(request: Request, response: Response) {
@@ -37,11 +50,15 @@ export async function update(request: Request, response: Response) {
   }
 
   updateSupplier(supplier);
-  response.status(200).json({ message: "Fornecedor atualizado com sucesso" });
+  return response
+    .status(200)
+    .json({ message: "Fornecedor atualizado com sucesso" });
 }
 
 export function exclude(request: Request, response: Response) {
   const { cnpj_number } = request.body;
   deleteSupplier(cnpj_number);
-  response.status(200).json({ message: "Fornecedor deletado com sucesso" });
+  return response
+    .status(200)
+    .json({ message: "Fornecedor deletado com sucesso" });
 }
